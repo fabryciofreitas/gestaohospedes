91fcce7059adf1dd59068a08e419446943b198a2
fabryciofreitas <fabryciobfr@gmail.com>
2026-07-09 00:25:11 -0300

    Frontend

--- src/test/java/com/hotel/gestaohospedes/tests/CheckInControllerIntegrationTest.java
+++ src/test/java/com/hotel/gestaohospedes/tests/CheckInControllerIntegrationTest.java
@@ -2,7 +2,7 @@ package com.hotel.gestaohospedes.tests;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.DynamicPropertyRegistry;
